0131e99c2c02aa4d5bc9479516d1ac2f17120cc8
Said BELLALI <said.bellali.pro@gmail.com>
2023-03-19 16:55:43 +0100

    port factorization

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(process.env.EXPOSED_PORT ? parseInt(process.env.EXPOSED_PORT) : 3000);
 }
 bootstrap();
